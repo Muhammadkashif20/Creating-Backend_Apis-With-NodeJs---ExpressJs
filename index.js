2030787003251719f55e2b58296a4c325163771b
muhammadkashif20 <kashif28@gmail.com>
2024-12-07 02:05:02 +0500

    Creating GET,POST,PUT,DELETE API'S with nodejs , express , using postman

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 import express from "express";
+// mock data:-
+const tasks = [
+  {
+    id: 1,
+    task: "why?",
+  },
+  {
+    id: 2,
+    task: "what is this?",
+  },
+  {
+    id: 3,
+    task: "hello world?",
+  },
+];
 const app = express();
 const PORT = 4000;
-app.get('/',(res,req)=>{
-    console.log('req=>',req);
-    res.send('Hello World My First Api Get Request!')
+app.get("/", (req, res) => {
+  res.status(200).send(tasks);
+});
+app.post("/", (req, res) => {
+  res.send("Hello World My Second Api Post Request!");
+});
+app.put('/',(req,res)=>{
+    res.send('Hello World My Third Api Put Request!')
 })
+app.delete("/", (req, res) => {
+  res.send("Hello World My Fourth Api Delete Request!");
+});
 // app.listen check karega ke konsi request arahi h.
-app.listen(PORT,()=>console.log('Server is Running on PORT '+PORT))
+app.listen(PORT, () => console.log("Server is Running on PORT ", PORT));
