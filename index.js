2aa8a702372c2d231687a5173580f4db16e7b6ec
muhammadkashif20 <kashif28@gmail.com>
2024-12-06 16:58:51 +0500

    Create my first api  working progress

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-console.log('Hello To Backend.');
+import express from "express";
+const app = express();
+const PORT = 4000;
+app.get('/',(res,req)=>{
+    console.log('req=>',req);
+    res.send('Hello World My First Api Get Request!')
+})
+// app.listen check karega ke konsi request arahi h.
+app.listen(PORT,()=>console.log('Server is Running on PORT '+PORT))
