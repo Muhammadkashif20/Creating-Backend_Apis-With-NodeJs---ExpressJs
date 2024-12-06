a8edfb965029d97bb76b8908048552c8a50c3d50
muhammadkashif20 <kashif28@gmail.com>
2024-12-07 02:55:32 +0500

    Morgan , express.json  , application middleware ,routermiddle using working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 // mock data:-
 const tasks = [
   {
@@ -16,16 +17,26 @@ const tasks = [
 ];
 const app = express();
 const PORT = 4000;
-
+app.use(morgan('tiny'))
 // Application-Level MiddleWare:
-
-app.get("/", (req, res) => {
+ const MiddleWare=(req,res,next)=>{
+    console.log("request=>",req.requestBy="Muhammad Kashif");
+    res.status(400).send("Few Error For Your System So Check This!")
+    // console.log("MiddleWare=>",new Date());
+    next()
+ }
+//  app.use(MiddleWare)
+app.use(express.json())
+app.get("/", MiddleWare,(req, res) => {
+    console.log(req.requestBy);
   res.status(200).send(tasks);
 });
 app.post("/", (req, res) => {
   res.send("Hello World My Second Api Post Request!");
 });
 app.put('/',(req,res)=>{
+    console.log("req body=>",req.body);
+    
     res.send('Hello World My Third Api Put Request!')
 })
 app.delete("/", (req, res) => {
