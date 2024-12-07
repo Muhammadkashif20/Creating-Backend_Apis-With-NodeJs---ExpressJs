04537475c6ed3bb42bd71ac273d0209e73c73f7f
muhammadkashif20 <kashif28@gmail.com>
2024-12-07 13:40:23 +0500

    practice on multiple middleware in node , expressjs with comments for use middleware

--- index.js
+++ index.js
@@ -17,15 +17,19 @@ const tasks = [
 ];
 const app = express();
 const PORT = 4000;
+//app.use(morgan('tiny')) Middleware ka use logging ke lye hota h!
 app.use(morgan('tiny'))
-// Application-Level MiddleWare:
+// Application-Level MiddleWare : ye wo hote jo lazmi har jaga chalte h.
+// Router-Level MiddleWare : ye wo hote jo chalte h lekin hm isko specific jaga bhi sirf use krsakte h.iski example line 34 pr h.
  const MiddleWare=(req,res,next)=>{
     console.log("request=>",req.requestBy="Muhammad Kashif");
-    res.status(400).send("Few Error For Your System So Check This!")
+    // res.status(400).send("Few Error For Your System So Check This!")
     // console.log("MiddleWare=>",new Date());
     next()
  }
 //  app.use(MiddleWare)
+
+// app.use(express.json()) Middleware ka use json format ko parse me convert krna h. 
 app.use(express.json())
 app.get("/", MiddleWare,(req, res) => {
     console.log(req.requestBy);
